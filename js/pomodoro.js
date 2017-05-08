5cce1eb5336de6859914dc397e32b3c561409164
Eli Tan <eliedrian1996@gmail.com>
2017-05-09 02:06:28 +0800

    Rewrite

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,25 +1,18 @@
 (function() {
-	var body = document.getElementsByTagName("body")[0];
-
 	var Timer = function() {
-		this.time = 0;
-		this.start = function(t, callback) {
-			this.time = t;
-			while (this.time >= 0) {
-				this.time--;
-			}
-			callback();
+		var startTime = Date.now();
+		var running = false;
+
+		this.reset = function() {
+			console.log("RESET");
+			console.log(startTime);
 		}
+
+		return {
+			reset: reset
+		};
 	};
 
-	var Color = function(r, g, b) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
+	Timer().reset();
 
-	var Tomato = function() {
-		this.ripeColor = new Color(245, 50, 41);
-		this.rawColor = new Color(41, 255, 41);
-	}
 })();
